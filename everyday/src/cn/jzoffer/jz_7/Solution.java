527dde3c418b53991585c2421bf5f2e9710f3711
yannnn <tgeuuy54@163.com>
2021-06-02 14:56:15 +0800

    ok

---
+++ everyday/src/cn/jzoffer/jz_7/Solution.java
@@ -0,0 +1,13 @@
+package cn.jzoffer.jz_7;
+
+import cn.structurel.TreeNode;
+import org.junit.Test;
+
+public class Solution {
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode();
+
+
+    }
+}
--- everyday/src/cn/structurel/ListNode.java
+++ everyday/src/cn/structurel/ListNode.java
@@ -4,14 +4,15 @@ public class ListNode {
     public  int val;
     public  ListNode next;
 
+    public ListNode() {
+    }
 
 
-
-    public  void ListNode(int val) {
+    public  ListNode(int val) {
         this.val = val;
     }
 
-    public  void ListNode(int val, ListNode next) {
+    public  ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
