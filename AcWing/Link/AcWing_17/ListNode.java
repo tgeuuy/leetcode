907d64b370e74578dcb7f9ca202abfff57fe718d
yannnn <tgeuuy54@163.com>
2021-06-17 10:37:53 +0800

    ok

--- AcWing/Link/AcWing_17/ListNode.java
+++
@@ -1,7 +0,0 @@
-package Link.AcWing_17;
-
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {this.val=x;}
-}
--- test/teat1.java
+++
@@ -1,5 +0,0 @@
-public class teat1 {
-    public static void main(String[] args) {
-        System.out.println(5 + 5 >> 1);
-    }
-}
