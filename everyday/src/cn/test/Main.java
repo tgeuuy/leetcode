93a306d9eed4210686af56d1f194c9f3e720fc94
tgeuuy <tgeuuy54@163.com>
2021-09-12 10:37:13 +0800

    20210912

---
+++ everyday/src/cn/test/Main.java
@@ -0,0 +1,12 @@
+package cn.test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("sssss");
+    }
+}
+class aaa{
+    public static void main(String[] args) {
+        System.out.println("ddddddd");
+    }
+}
