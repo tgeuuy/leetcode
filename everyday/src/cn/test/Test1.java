9f808aa59e8680cb2fab4a0caad0ef4ba3ad91a8
tgeuuy <tgeuuy54@163.com>
2021-10-31 15:34:54 +0800

    20211031

--- everyday/src/cn/test/Test1.java
+++ everyday/src/cn/test/Test1.java
@@ -1,6 +1,10 @@
 package cn.test;
 
 public class Test1 {
+
+    public static final int M = 7;
+    public final int MDAD = 4;
+
     public static void main(String[] args) {
         int c = 5, b = 9;
         int d = c++;
@@ -10,5 +14,6 @@ public class Test1 {
 //        System.out.println(++b);
         System.out.println(d);
         System.out.println(e);
+        System.out.println(M);
     }
 }
