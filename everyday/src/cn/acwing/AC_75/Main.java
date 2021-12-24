d0cde641aaa386201ab8c1b08acc858c11bd28fa
tgeuuy <tgeuuy54@163.com>
2021-12-24 10:05:01 +0800

    20211224

--- everyday/src/cn/acwing/AC_75/Main.java
+++ everyday/src/cn/acwing/AC_75/Main.java
@@ -2,6 +2,7 @@ package cn.acwing.AC_75;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author tgeuuy
@@ -11,26 +12,20 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4};
-        int sum = 7;
-        int[] res = findNumbersWithSum(nums, sum);
+        int[] nums = {2, 0, 9, 8, 5, 0, 0, 7, 0};
+        int[] res = findNumbersWithSum(nums, 17);
         System.out.println(Arrays.toString(res));
-
     }
-
 
     public static int[] findNumbersWithSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        if (nums.length < 2) return null;
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int val = target - nums[i];
-            if (map.containsKey(val)) {
-                return new int[]{nums[i], val};
-            } else {
-                map.put(nums[i], i);
-            }
-
+            map.put(nums[i], i);
+            if (map.containsKey(target - nums[i]))
+                return new int[]{nums[i], target - nums[i]};
         }
         return null;
-
     }
+
 }
---
+++ everyday/src/cn/acwing/AC_77/Main.java
@@ -0,0 +1,47 @@
+package cn.acwing.AC_77;
+
+import java.util.Arrays;
+
+/**
+ * @Author tgeuuy
+ * @Date 2021/12/17 10:21
+ * @Version 1.0
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        String s = "I am a student.";
+        String res = reverseWords(s);
+        System.out.println("res = " + res);
+    }
+
+    public static String reverseWords(String s) {
+        s = s.trim();
+        char[] chars = s.toCharArray();
+
+        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (chars[i] == ' ') {
+                reverse(chars, index, i - 1);
+                index = i + 1;
+            }
+            if (i == s.length() - 1) {
+                reverse(chars, index, i);
+            }
+        }
+        reverse(chars, 0, s.length() - 1);
+        return new String(chars);
+
+    }
+    private static void reverse(char[] chars, int start, int end) {
+        if (start > end) return;
+        while (start < end) {
+            char ch = chars[start];
+            chars[start] = chars[end];
+            chars[end] = ch;
+            start++;
+            end--;
+        }
+    }
+}
